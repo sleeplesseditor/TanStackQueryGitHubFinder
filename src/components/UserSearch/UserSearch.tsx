1dc70b01cb156bf86466c683816d68f6bde8e07c
sleeplesseditor <josh@bryson.me.uk>
2026-05-01 15:52:02 +0100

    Updated to use of grid layout and sticky header

--- src/components/UserSearch/UserSearch.tsx
+++ src/components/UserSearch/UserSearch.tsx
@@ -33,6 +33,7 @@ const UserSearch = () => {
     console.log('Data:', data);
 
     return (
+        <>
         <div className="user-search__container">
             <form
                 className="user-search__form"
@@ -52,28 +53,33 @@ const UserSearch = () => {
                 </button>
             </form>
             {isLoading && <p className="loading-message">Loading...</p>}
-            {error && <p className="error-message">Error: {error.message}</p>}
-            {data && data.items.map((user: any) => (
-                <div className="user-search__result">
-                    <img 
-                        alt={`${user.login}'s avatar`} 
-                        className="user-search__avatar" 
-                        src={user.avatar_url}
-                        width={100} 
-                    />
-                    <h2 className="user-search__name">{user.name || user.login}</h2>
-                    <p className="user-search__bio">{user.bio}</p>
-                    <a 
-                        className="user-search__profile-link"
-                        href={user.html_url} 
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        <FaGithubAlt /> View Profile
-                    </a>
-                </div>
-            ))}
+            {error && <p className="error-message">Error: {error.message}</p>}  
         </div>
+            {data?.items.length > 0 && (
+                <div className="user-search__results-container">
+                    {data.items.map((user: any) => (
+                        <div className="user-search__result">
+                            <img 
+                                alt={`${user.login}'s avatar`} 
+                                className="user-search__avatar" 
+                                src={user.avatar_url}
+                                width={100} 
+                            />
+                            <h2 className="user-search__name">{user.name || user.login}</h2>
+                            <p className="user-search__bio">{user.bio}</p>
+                            <a 
+                                className="user-search__profile-link"
+                                href={user.html_url} 
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <FaGithubAlt /> View Profile
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )}  
+        </>
     )
 };
 
